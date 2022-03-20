9b1f9fff8b25fb74482ac5a7bfbce2f8a479bca2
artyom-kudriavtsev <artem.kudryavcev@sravni.ru>
2022-03-20 15:17:56 +0300

    [DT-5]: add tests

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-helm install order ./app -f ./order-service/values.yaml
-helm install payment ./app -f ./payment-service/values.yaml
-helm install delivery ./app -f ./delivery-service/values.yaml
-helm install warehouse ./app -f ./warehouse-service/values.yaml
-helm install coordinator ./app -f ./coordinator-service/values.yaml
--- stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-helm un order
-helm un payment
-helm un delivery
-helm un warehouse
-helm un coordinator
-
-#kubectl delete all --all
